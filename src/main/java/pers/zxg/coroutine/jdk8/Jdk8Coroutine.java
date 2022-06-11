7926e859af25bc1c81326580d9d98517c237accc
Xianguang-Zhou <xianguang.zhou@outlook.com>
2022-06-11 08:46:24 +0800

    Improve.

--- src/main/java/pers/zxg/coroutine/jdk8/Jdk8Coroutine.java
+++ src/main/java/pers/zxg/coroutine/jdk8/Jdk8Coroutine.java
@@ -5,8 +5,11 @@ package pers.zxg.coroutine.jdk8;
 
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
+import pers.zxg.coroutine.Coroutine.Callable;
 import pers.zxg.coroutine.Coroutine.OutsideCall;
+import pers.zxg.coroutine.Coroutine.Runnable;
 import pers.zxg.coroutine.Coroutine.Suspend;
 import pers.zxg.coroutine.Coroutine.Weaver.NotWeaved;
 
@@ -16,6 +19,14 @@ import pers.zxg.coroutine.Coroutine.Weaver.NotWeaved;
 public class Jdk8Coroutine {
 
 	public static <T> T await(CompletionStage<T> stage) throws Suspend, ExecutionException, OutsideCall {
+		throw new NotWeaved();
+	}
+
+	public static Future<Void> run(Runnable runnable) {
+		throw new NotWeaved();
+	}
+
+	public static <T> Future<T> run(Callable<T> callable) {
 		throw new NotWeaved();
 	}
 }
